5ea840bc63210243c82218f2d65769a9d4a5e449
Alex Auch <alexsamuelauch@gmail.com>
2022-12-12 16:10:05 -0700

    fixed rewards models

--- app/models/reward.py
+++ app/models/reward.py
@@ -14,13 +14,12 @@ class Reward(db.Model):
     quantity = db.Column(db.Integer, nullable=False)
     price_threshold = db.Column(db.Integer, nullable=False)
     shipping_date = db.Column(db.DateTime, nullable=False)
-    description = db.Column(db.String(50000), nullable=False)
-
-
+    ships_to = db.Column(db.String(50), nullable=False)
     project = db.relationship('Project', back_populates='reward')
-
-    user = db.relationship('User', secondary=backing_table,
-                           back_populates='reward')
+    user = db.relationship(
+        'User', secondary=backing_table, back_populates='reward')
+    includes = db.Column(db.String(500), nullable=False)
+    description = db.Column(db.String(50000), nullable=False)
 
     def to_dict(self):
         return {
@@ -30,31 +29,7 @@ class Reward(db.Model):
             'quantity': self.quantity,
             'price_threshold': self.price_threshold,
             'shipping_date': self.shipping_date,
-            'description': self.description
+            'description': self.description,
+            'includes': self.includes,
+            'ships_to': self.ships_to
         }
-
-  id = db.Column(db.Integer, primary_key=True, nullable=False)
-  project_id = db.Column(db.Integer, ForeignKey('projects.id'), nullable=False)
-  name = db.Column(db.String(50), nullable=False)
-  quantity = db.Column(db.Integer, nullable=False)
-  price_threshold = db.Column(db.Integer, nullable=False)
-  shipping_date = db.Column(db.DateTime, nullable=False)
-  ships_to = db.Column(db.String(50), nullable=False)
-  project = db.relationship('Project', back_populates='reward')
-  user = db.relationship('User', secondary=backing_table, back_populates='reward')
-  includes = db.Column(db.String(500), nullable=False)
-  description = db.Column(db.String(50000), nullable=False)
-
-  def to_dict(self):
-    return {
-      'id': self.id,
-      'project_id': self.project_id,
-      'name': self.name,
-      'quantity': self.quantity,
-      'price_threshold': self.price_threshold,
-      'shipping_date': self.shipping_date,
-      'description': self.description,
-      'includes' : self.includes,
-      'ships_to' : self.ships_to
-    }
-
