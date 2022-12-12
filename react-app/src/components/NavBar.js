4ebad29c69bd5db797887e56776c4b8566167718
hoon <isaacki1003@gmail.com>
2022-12-12 15:14:11 -0800

    test

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,9 +1,31 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 
 const NavBar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  if (isLoggedIn) {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/users' exact={true} activeClassName='active'>
+              Users
+            </NavLink>
+          </li>
+          <li>
+            <LogoutButton onClick={() => setIsLoggedIn(false)} />
+          </li>
+        </ul>
+      </nav>
+    );
+  }
   return (
     <nav>
       <ul>
@@ -21,14 +43,6 @@ const NavBar = () => {
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
         </li>
       </ul>
     </nav>
