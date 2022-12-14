943f1515a113cf7529b31570824365ec63ff7955
Alex Auch <alexsamuelauch@gmail.com>
2022-12-14 10:27:20 -0700

    added event listener

--- react-app/src/components/SearchBar.js
+++ react-app/src/components/SearchBar.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom"
 import "../css/NavBar.css"
 function SearchBar() {
     const [searchParams, setSearchParams] = useState('')
+    const history = useHistory()
 
     const handleSubmit = (e) => {
-        e.preventDefault()
+        history.push({
+            pathname: '/discover',
+            search: `?${searchParams}`
+        })
     }
 
+    useEffect(() => {
+        const enterSubmit = e => {
+            if (e.code === "Enter") {
+                e.preventDefault()
+                handleSubmit()
+            }
+        }
+        document.addEventListener('keydown', enterSubmit)
+        return () => document.removeEventListener('keydown', enterSubmit)
+    }, [])
     console.log(searchParams)
     return (
         <form onSubmit={handleSubmit}>
@@ -19,7 +33,6 @@ function SearchBar() {
                 onChange={e => setSearchParams(e.target.value)}
             />
         </form>
-
     )
 }
 
