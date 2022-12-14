5891fbd7c2da9a219bc367ffadee37d57dce5c33
Alex Auch <alexsamuelauch@gmail.com>
2022-12-13 23:28:50 -0700

    was implmenting search

---
+++ react-app/src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/NavBar.css"
+function SearchBar() {
+
+    return (
+
+        <textarea className="search-bar" />
+
+    )
+}
+
+export default SearchBar
