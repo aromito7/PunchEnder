9d1b5d2f1d5f30211ce994a0a03047b7f0a4014f
hoon <isaacki1003@gmail.com>
2022-12-13 16:23:44 -0800

    initial landing

--- react-app/src/components/home/FeaturedProduct.js
+++ react-app/src/components/home/FeaturedProduct.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const FeaturedProduct = () => {
+    const [project, setProject] = useState({});
+
+    useEffect(() => {
+        (async () => {
+        const res = await fetch(`/api/projects/1`);
+        const data = await res.json();
+        console.log("THE PROJECT DATA ----------> ", data)
+        setProject(data);
+        })();
+    }, []);
+
     return (
         <div>
-        <NavLink to="/projects/1" photoURL="https://www.kickstarter.com/projects/compassonstage/compass-0?ref=section-homepage-featured-project">Featured Project</NavLink>
+            <div>
+                Featured Product
+            </div>
+        <NavLink to="/projects/1">
+            <img src="https://ksr-ugc.imgix.net/assets/039/359/210/ecde97b50494f1ac56df06ef17a4de10_original.jpg?ixlib=rb-4.0.2&w=680&fit=max&v=16698268
+            19&gif-q=50&q=92&s=91599c28af28e843daae40c20d2f31a2" alt="test" width="600em" height="300em"></img>
+            <div>
+                {project.name}
+            </div>
+            <div>
+                {project.short_description}
+            </div>
+            <div>
+             by {project.owner_id}
+            </div>
+        </NavLink>
         {/* Need to add name and short description */}
         </div>
     );
--- react-app/src/components/home/InfoBar.js
+++ react-app/src/components/home/InfoBar.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams, NavLink, useHistory } from "react-router-dom";
+
 
 const InfoBar = () => {
+    const [projects, setProjects] = useState({});
+    const [projectsFunded, setProjectsFunded] = useState(0);
+    const [total, setTotal] = useState(0);
+    const [totalPledges, setTotalPledges] = useState(0);
+
+    useEffect(() => {
+        (async () => {
+            const res = await fetch('/api/projects');
+            const data = await res.json();
+            console.log("THE PROJECT DATA ----------> ", data)
+            setProjects(data.projects);
+            let total = 0;
+            for (let project of Object.values(projects)) {
+                total += project.current_amount;
+            }
+            setTotal(total);
+        })();
+    }, []);
+
+    function numberWithCommas(x){
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
     return (
         <div className='info-bar-wrapper'>
            <div className='info-bar-box'>
-            *INSERT TOTAL* projects funded
+                <div className='green-giant'>
+                    {/* {numberWithCommas(projects.length)} */}
+                </div>
+                <div>
+                    projects funded
+                </div>
+           </div>
+
+           <div className='info-bar-box'>
+                <div className='green-giant'>
+                    ${numberWithCommas(total)}
+                </div>
+                <div>
+                    towards creative work
+                </div>
            </div>
            <div className='info-bar-box'>
-            *INSERT TOTAL* towards creative work
-           </div>
-           <div className='info-bar-box'>
-            *INSERT TOTAL* pledges
+                <div className='green-giant'>
+                    {numberWithCommas(99999999999999)}
+                </div>
+                <div>
+                pledges
+                </div>
            </div>
         </div>
     );
