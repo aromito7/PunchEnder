8ce9c44a440f05c729c710fc13c612e8e7384b90
Alex Auch <alexsamuelauch@gmail.com>
2022-12-14 17:14:38 -0700

    added missing foreign key helper function for production

--- app/models/backing.py
+++ app/models/backing.py
@@ -5,8 +5,8 @@ from sqlalchemy.schema import Column, ForeignKey, Table
 backing_table = Table('backings',
                       db.Model.metadata,
                       Column("user_id", ForeignKey(
-                          "users.id"), primary_key=True),
-                      Column("reward_id", ForeignKey("rewards.id"), primary_key=True))
+                          add_prefix_for_prod("users.id")), primary_key=True),
+                      Column("reward_id", ForeignKey(add_prefix_for_prod("rewards.id")), primary_key=True))
 
 if environment == "production":
     backing_table.schema = SCHEMA
