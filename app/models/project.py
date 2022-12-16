cd4e0fe15d5d25a4199f290b5aeace48ecbc6d46
user.email <aromito7@gmail.com>
2022-12-16 14:06:08 -0500

    Fixed indentations in models

--- app/models/project.py
+++ app/models/project.py
@@ -10,8 +10,7 @@ class Project(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer, ForeignKey(
-        add_prefix_for_prod('users.id')), nullable=False)
+    owner_id = db.Column(db.Integer, ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     name = db.Column(db.String(50), nullable=False)
     goal_amount = db.Column(db.Integer, nullable=False)
     current_amount = db.Column(db.Integer, nullable=False)
@@ -24,25 +23,24 @@ class Project(db.Model):
     state = db.Column(db.String(2), nullable=False)
 
     owner = db.relationship('User', back_populates='project')
-    reward = db.relationship(
-        'Reward', back_populates='project', cascade="all, delete")
+    reward = db.relationship('Reward', back_populates='project', cascade="all, delete")
     categories = db.relationship('Category', secondary=category_table, back_populates='projects')
 
 
     def to_dict(self):
-      return {
-        'id': self.id,
-        'owner_id': self.owner_id,
-        'categories': [category.name for category in self.categories],
-        'name': self.name,
-        'goal_amount': self.goal_amount,
-        'current_amount': self.current_amount,
-        'start_date': self.start_date.isoformat(),
-        'end_date': self.end_date.isoformat(),
-        'short_description': self.short_description,
-        'long_description': self.long_description,
-        'preview_image': self.preview_image,
-        'city': self.city,
-        'state': self.state,
-        'rewards': [r.to_dict() for r in self.reward],
-      }
+        return {
+            'id': self.id,
+            'owner_id': self.owner_id,
+            'categories': [category.name for category in self.categories],
+            'name': self.name,
+            'goal_amount': self.goal_amount,
+            'current_amount': self.current_amount,
+            'start_date': self.start_date.isoformat(),
+            'end_date': self.end_date.isoformat(),
+            'short_description': self.short_description,
+            'long_description': self.long_description,
+            'preview_image': self.preview_image,
+            'city': self.city,
+            'state': self.state,
+            'rewards': [r.to_dict() for r in self.reward],
+        }
